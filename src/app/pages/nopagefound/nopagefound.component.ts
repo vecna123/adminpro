6ab5d9703949a161d4dd240807cdfc6f2e83a949
Josué Vera <josue.vera@efby.cl>
2023-09-27 11:59:01 -0300

    proyecto adminpro curso angular anvanzado

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.css']
+})
+export class NopagefoundComponent {
+
+public year:Date = new Date()
+
+
+}
